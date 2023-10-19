878f7e9e4fc21c2830e54c1a33166c9c9109db74
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-10-19 14:57:09 +0200

    Add heap listener

--- LedBlinker/Main.cpp
+++ LedBlinker/Main.cpp
@@ -13,7 +13,7 @@
 #include <zephyr/fs/littlefs.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/storage/flash_map.h>
-
+#include <zephyr/sys/heap_listener.h>
 #include <zephyr/sys_clock.h>
 
 #include <zephyr/drivers/gpio.h>
@@ -28,6 +28,9 @@ FS_FSTAB_DECLARE_ENTRY(PARTITION_NODE);
 static struct gpio_dt_spec spi_nhold_pin = GPIO_DT_SPEC_GET(SPI_NHOLD_NODE, gpios);
 static struct gpio_dt_spec spi_nwp_pin = GPIO_DT_SPEC_GET(SPI_NWP_NODE, gpios);
 static struct fs_mount_t *mp = &FS_FSTAB_ENTRY(PARTITION_NODE);
+
+LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
+
 
 // Mounts LittleFS on the MRAM.
 // Returns 0 on success, negative on failure.
@@ -69,11 +72,27 @@ int mountFilesystem(bool shouldWipe) {
     return 0;
 }
 
+extern struct sys_heap _system_heap;
+static uint32_t total_allocated = 0;
+// Alternatively, `sys_heap_runtime_stats_get()` could be used:
+// https://github.com/zephyrproject-rtos/zephyr/blob/cfe667deb894a7f9cf67cfdae8a30824f59927e8/subsys/shell/modules/kernel_service.c#L282
+void on_heap_alloc(uintptr_t heap_id, void *mem, size_t bytes)
+{
+    total_allocated += bytes;
+    // LOG_INF("Memory allocated from heap %" PRIuPTR " at %p, size %u, total %u", heap_id, mem, bytes, total_allocated);
+    printk("%u, %u\n", bytes, total_allocated);
+    // LOG_ERR("Heap allo %u, total %u", bytes, total_allocated);
+}
+HEAP_LISTENER_ALLOC_DEFINE(my_listener, HEAP_ID_FROM_POINTER(&_system_heap), on_heap_alloc);
+
+
 int main()
 {
-    if (mountFilesystem(/*shouldWipe=*/false) < 0) {
+    if (mountFilesystem(/*shouldWipe=*/true) < 0) {
         return 1;
     }
+    
+    heap_listener_register(&my_listener);
 
     LedBlinker::TopologyState inputs;
     printk("Setting up topology\n");
