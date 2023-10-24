0bbff6b0d690c6333b72623c91bd9541784bdd94
Roman Sobkuliak <r.sobkuliak@gmail.com>
2023-10-24 20:27:17 +0200

    Add usage

--- LedBlinker/bench/perf_macros.h
+++ LedBlinker/bench/perf_macros.h
@@ -4,6 +4,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/kernel/thread.h>
 
+// Usage example:
+// int send_data(uint8_t *data, int len) {
+//     PERF_SETUP(uart_write)
+    
+//     PERF_MEASURE_START(uart_write)
+//     for (int i = 0; i < len; ++i) {
+//         uart_write(uart_dev, data, 1);
+//     }
+//     PERF_MEASURE_END(uart_write)
+
+//     // Print result every 5 seconds on a 550MHz CPU.
+//     PERF_PRINT(uart_write, 5 * 550 * 1000 * 1000)
+// }
+
+
 #if PERF_ENABLED == 0
 #define PERF_SETUP(name)
 #define PERF_MEASURE_START(name)
@@ -36,7 +51,7 @@
     name##_perf_ret = k_thread_runtime_stats_all_get(&name##_perf_rt_stats_all); \
     FW_ASSERT(name##_perf_ret == 0); \
     if (name##_perf_rt_stats_all.execution_cycles - name##_perf_total_used_cycles_started > cycles_timeout) { \
-        printk("Bench " #name " result: %.2f, Raw: %llu\n", 100 * (double) name##_used_cycles / (name##_perf_rt_stats_all.execution_cycles - name##_perf_total_used_cycles_started), name##_used_cycles); \
+        printk("Bench " #name " result: %.2f, Raw: %llu\n / %llu", 100 * (double) name##_used_cycles / (name##_perf_rt_stats_all.execution_cycles - name##_perf_total_used_cycles_started), name##_used_cycles, (name##_perf_rt_stats_all.execution_cycles - name##_perf_total_used_cycles_started)); \
         name##_used_cycles = 0; \
         name##_perf_total_used_cycles_started = name##_perf_rt_stats_all.execution_cycles; \
     }
